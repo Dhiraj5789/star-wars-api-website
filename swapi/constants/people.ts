a6004c82673956eff2ec3d02104fbbe041500b9e
Dhiraj Suthar <dhirajsuthar@L-FVFGPAPVQ05N.local>
2022-07-23 17:38:31 +0530

    mapped characters, css cards to be created

---
+++ swapi/constants/people.ts
@@ -0,0 +1,5 @@
+export const peopleImages = ["https://ucarecdn.com/c76452e8-a2e0-4d0b-9585-79932750ecc8/",
+    "https://ucarecdn.com/e01f730a-2b9f-44bc-91ce-01d4e7f427a6/",
+    "https://ucarecdn.com/a76855a4-c922-4ed6-bd43-08fb22e15f8b/",
+    "https://ucarecdn.com/47b4052c-7468-4326-bbcd-817b28f28e4f/",
+    "https://ucarecdn.com/86d1071a-dad2-4230-8f14-a28786d8d9df/"];
--- swapi/pages/index.tsx
+++ swapi/pages/index.tsx
@@ -1,9 +1,29 @@
 import type { NextPage } from 'next'
+import Image from "next/image";
+import { peopleImages } from '../constants/people';
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+  const { sl }:any = props;
   return (
-    <h1>Star Wars</h1>
+    <><h1>Star Wars</h1>
+    {sl.map((person:any)=><div key={person.name}>
+<Image src={person.image} width={100} height={100} alt={person.name}/>
+<h3>{person.name}</h3>
+</div>)}
+    </> 
   )
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const people = "https://swapi.dev/api/people/";
+  const res = await fetch(people);
+  const data = await res.json()
+  const sl = data.results.slice(0,5);
+  sl.forEach((person:any, idx:number)=> {person.image = peopleImages[idx]})
+  console.log(sl);
+  // Pass data to the page via props
+  return { props: { sl } }
 }
 
 export default Home
